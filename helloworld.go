73a18fbaf20589fe7da17f8b2ca46a87c51f3868
@networkpankaj <@networkpankaj>
2024-05-23 12:28:51 +0000

    third Init

--- helloworld.go
+++ helloworld.go
@@ -13,7 +13,6 @@ func main() {
 	var tax float64 = taxRate * float64(price)
 
 	fmt.Println("Tax is", tax, "dollars")
-
-	var total float64 = float64(price) + float64(tax)
+	var total float64 = float64(price) + tax
 	fmt.Println("Total cost is", total, "dollars")
 }
